9762ec2c04d86a6cc807c042a622098452a3b15d
daoudahmad <daoud@hotmail.dk>
2023-11-09 13:20:03 +0100

    ddd

--- RestPair/Program.cs
+++ RestPair/Program.cs
@@ -1,3 +1,4 @@
+using RestPair;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +7,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<MusicRecordsRepo>();
 
 var app = builder.Build();
 
